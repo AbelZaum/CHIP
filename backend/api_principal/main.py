672cf55d7d8a05478bd944b94ceceb2382da4615
Gabriel Abel da Silva <127804266+AbelZaum@users.noreply.github.com>
2025-08-15 14:02:42 -0300

    Update main.py

--- backend/api_principal/main.py
+++ backend/api_principal/main.py
@@ -17,7 +17,7 @@ from typing import Optional
 app = FastAPI()
 
 # --- Configuração do CORS ---
-origins = ["http://localhost:5500", "http://127.0.0.1:5500", "null"]
+origins = ["http://localhost:5500", "http://127.0.0.1:5500", "null", "34.46.184.53"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # --- Modelos de Dados ---
